e88fdf96f8347b38902707b6f87efbba333bae11
William Kristiansen <wjkristiansen@hotmail.com>
2021-12-28 22:47:05 -0800

    2021: Day16: Solve packed decoder

--- 2021/src/day16.cpp
+++ 2021/src/day16.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 #include <assert.h>
 
@@ -8,20 +9,6 @@
 
 namespace Day16
 {
-	const uint8_t TestData[] =
-	{
-		0b10110100,
-		0b00110011,
-		0b10011010,
-		0b00001111,
-		0b10010000,
-		0b00100111,
-		0b11110010,
-	};
-
-//110011100110100
-//00011111
-
 	class BitStream
 	{
 	public:
@@ -33,6 +20,8 @@ namespace Day16
 		BitStream(const uint8_t* pBuffer, size_t bufferSize) :
 			m_pBuffer(pBuffer),
 			m_BufferSize(bufferSize) {}
+
+		size_t GetOffsetInBits() const { return m_ByteOffset * 8 + m_BitOffset; }
 	};
 
 	class BitStreamReader
@@ -45,8 +34,10 @@ namespace Day16
 		{
 		}
 
-		size_t Read(uint8_t* pBuffer, size_t numBits, size_t bufferSize)
+		size_t Read(void* pOutputBuffer, size_t numBits, size_t bufferSize)
 		{
+			size_t result = numBits;
+			uint8_t* pBuffer = reinterpret_cast<uint8_t*>(pOutputBuffer);
 			if (numBits == 0)
 				return 0;
 
@@ -61,11 +52,11 @@ namespace Day16
 				// Read from the current byte
 				uint8_t NumReadBits = std::min<uint8_t>(ChunkSize, 8 - m_BitStream.m_BitOffset);
 				uint8_t RightShift = uint8_t(std::max<int>(0, 8 - m_BitStream.m_BitOffset - ChunkSize));
-				uint8_t CurByte = (m_BitStream.m_pBuffer[m_BitStream.m_ByteOffset]) >> RightShift;
+				uint8_t Mask = 0xff >> m_BitStream.m_BitOffset;
+				uint8_t CurByte = (Mask & (m_BitStream.m_pBuffer[m_BitStream.m_ByteOffset])) >> RightShift;
 				uint8_t LeftShift = ChunkSize - NumReadBits;
 				numBits -= NumReadBits;
-				uint8_t Mask = 0xff >> m_BitStream.m_BitOffset;
-				pBuffer[BufferIndex] |= ((Mask & CurByte) << LeftShift);
+				pBuffer[BufferIndex] |= (CurByte << LeftShift);
 
 				// Update the stream state 
 				size_t NewBitOffset = m_BitStream.m_BitOffset + NumReadBits;
@@ -86,23 +77,144 @@ namespace Day16
 				}
 			}
 
-			return BufferIndex;
+			return result;
 		}
+	};
+
+	class PacketDecoder
+	{
+		BitStream m_Stream;
+		BitStreamReader m_Reader;
+		size_t m_SumOfVersions = 0;
+
+		uint8_t ReadUInt8(size_t numBits)
+		{
+			uint8_t Value = 0;
+			if (0 == m_Reader.Read(&Value, numBits, sizeof(Value)))
+				throw std::exception();
+			return Value;
+		}
+
+		uint16_t ReadUInt16(size_t numBits)
+		{
+			uint16_t Value = 0;
+			if (0 == m_Reader.Read(&Value, numBits, sizeof(Value)))
+				throw std::exception();
+			return Value;
+		}
+
+		uint64_t ReadLiteral()
+		{
+			uint64_t Literal = 0;
+			uint8_t NumNibbles = 0;
+			uint8_t Nibbles[16] = { 0 };
+			for (NumNibbles = 0;;)
+			{
+				uint8_t Value = ReadUInt8(5);
+				Nibbles[NumNibbles] = Value & 0x0f;
+				++NumNibbles;
+				if (!(Value & 0x10))
+					break;
+			}
+			int Shift = 4 * (NumNibbles - 1);
+			for (int i = 0; i < NumNibbles; ++i)
+			{
+				Literal |= Nibbles[i] << Shift;
+				Shift -= 4;
+			}
+			return Literal;
+		}
+
+		int ReadVersion()
+		{
+			return ReadUInt8(3);
+		}
+
+		int ReadTypeId()
+		{
+			return ReadUInt8(3);
+		}
+
+		void ReadPacket(const std::string &indent, bool IsSubPacket)
+		{
+			int Version = ReadVersion();
+			m_SumOfVersions += Version;
+			int Type = ReadTypeId();
+			if(IsSubPacket)
+				std::cout << indent << "Begin SubPacket" << std::endl;
+			else
+				std::cout << indent << "Begin Packet" << std::endl;
+			std::cout << indent << " -Version: " << Version << std::endl;
+			std::cout << indent << " -TypeId: " << Type << std::endl;
+
+			switch (Type)
+			{
+			case 4: {
+				uint64_t Literal = ReadLiteral();
+				std::cout << indent << "    Literal: " << Literal << std::endl;
+				break; }
+			default: {
+				// Operator packet
+				int SubTypeId = ReadUInt8(1);
+				std::string SubIndent = indent + "  ";
+				if (SubTypeId == 0)
+				{
+					uint16_t SubPacketSizeInBits = ReadUInt16(15);
+					size_t CurOffsetInBits = m_Stream.GetOffsetInBits();
+					size_t EndOffsetInBits = CurOffsetInBits + SubPacketSizeInBits;
+					for (;m_Stream.GetOffsetInBits() < EndOffsetInBits;)
+					{
+						ReadPacket(SubIndent, true);
+					}
+				}
+				else
+				{
+					uint16_t NumSubPackets = ReadUInt16(11);
+
+					for (uint16_t subIndex = 0; subIndex < NumSubPackets; ++subIndex)
+					{
+						ReadPacket(SubIndent, true);
+					}
+				}
+
+				break; }
+			}
+		}
+
+	public:
+		PacketDecoder(uint8_t *pData, size_t dataSize) :
+			m_Stream(pData, dataSize),
+			m_Reader(m_Stream)
+		{
+		}
+
+		void Execute()
+		{
+			try
+			{
+				ReadPacket("", false); // throws when stream is fully parsed
+			}
+			catch (std::exception& )
+			{
+
+			}
+		}
+
+		size_t SumOfVersions() const { return m_SumOfVersions; }
 	};
 
 	void Execute()
 	{
-		BitStream Stream(TestData, sizeof(TestData));
-		BitStreamReader Reader(Stream);
-		uint8_t Buffer[16];
-		Reader.Read(Buffer, 3, sizeof(Buffer));
-		assert(Buffer[0] == 0x5);
-		Reader.Read(Buffer, 7, sizeof(Buffer));
-		assert(Buffer[0] == 0x50);
-		Reader.Read(Buffer, 15, sizeof(Buffer));
-		assert(Buffer[0] == 0x34);
-		assert(Buffer[1] == 0x67);
-		Reader.Read(Buffer, 8, sizeof(Buffer));
-		assert(Buffer[0] == 0x1f);
+		// Convert the input data from a string to a buffer of bytes
+		std::vector<uint8_t> m_Data;
+		const size_t Len = sizeof(DataString) - 1;
+		for (size_t i = 0; i < Len; i += 2)
+		{
+			m_Data.push_back(static_cast<uint8_t>(std::stoi(std::string(&DataString[i], 2), nullptr, 16)));
+		}
+
+		PacketDecoder Decoder(m_Data.data(), m_Data.size());
+		Decoder.Execute();
+		std::cout << "SumOfVersions=" << Decoder.SumOfVersions();
 	}
 }
