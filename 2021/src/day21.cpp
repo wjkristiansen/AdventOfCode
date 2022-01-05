ac1f5edb7d1428052e8cf9cb0961149325a156b2
William Kristiansen <wjkristiansen@hotmail.com>
2022-01-05 09:19:10 -0800

    2021: Day21: Part2: Solve Dirac Dice

--- 2021/src/day21.cpp
+++ 2021/src/day21.cpp
@@ -1,68 +1,141 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <assert.h>
 
 #include "day21.h"
 
 namespace Day21
 {
-    struct Player
+    // Per turn, there are 27 (3^3) possible roll results a 3-sided 'quantum' die
+    static const int RollResults[] =
     {
-        size_t Score = 0;
-        int Position = 0;
+        1 + 1 + 1,
+        1 + 1 + 2,
+        1 + 1 + 3,
+        1 + 2 + 1,
+        1 + 2 + 2,
+        1 + 2 + 3,
+        1 + 3 + 1,
+        1 + 3 + 2,
+        1 + 3 + 3,
+        2 + 1 + 1,
+        2 + 1 + 2,
+        2 + 1 + 3,
+        2 + 2 + 1,
+        2 + 2 + 2,
+        2 + 2 + 3,
+        2 + 3 + 1,
+        2 + 3 + 2,
+        2 + 3 + 3,
+        3 + 1 + 1,
+        3 + 1 + 2, 
+        3 + 1 + 3,
+        3 + 2 + 1,
+        3 + 2 + 2,
+        3 + 2 + 3,
+        3 + 3 + 1,
+        3 + 3 + 2,
+        3 + 3 + 3,
     };
+
+    static const int NewUniversesPerTurn = _countof(RollResults);
+    static const int WinningScore = 21;
+    static const uint8_t Player1Start = 6;
+    static const uint8_t Player2Start = 9;
 
     class Game
     {
-        int m_DetValueLast = 0;
-        int m_NextPlayer = 0;
-        int m_RollCount = 0;
-        Player m_Players[2];
+        std::unordered_map<uint32_t, uint8_t> m_EndPosTable;
+        std::unordered_map<uint32_t, std::pair<size_t, size_t>> m_WinCountsTable;
 
     public:
-        Game(int p1, int p2)
+        Game()
         {
-            m_Players[0].Position = p1;
-            m_Players[1].Position = p2;
-        }
-        int Roll()
-        {
-            m_RollCount += 3;
-            int result = 0;
-            m_DetValueLast = (m_DetValueLast % 100) + 1;
-            result += m_DetValueLast;
-            m_DetValueLast = (m_DetValueLast % 100) + 1;
-            result += m_DetValueLast;
-            m_DetValueLast = (m_DetValueLast % 100) + 1;
-            result += m_DetValueLast;
-            return result;
-        }
-        
-        int Turn()
-        {
-            int move = Roll();
-            m_Players[m_NextPlayer].Position = (((m_Players[m_NextPlayer].Position - 1) + move) % 10) + 1;
-            m_Players[m_NextPlayer].Score += m_Players[m_NextPlayer].Position;
-            if( m_Players[m_NextPlayer].Score >= 1000 )
-                return m_NextPlayer;
-            m_NextPlayer = m_NextPlayer ^ 1;
-            return -1;
         }
 
-        void Run()
+        static constexpr uint8_t EndPos(uint8_t StartPos, int RollValue)
         {
-            int Winner = -1;
-            while (Winner == -1) { Winner = Turn(); }
-            size_t Product = m_Players[(Winner ^ 1)].Score * m_RollCount;
-            std::cout << "Roll Count: " << m_RollCount << std::endl;
-            std::cout << "Player 1 Score: " << m_Players[0].Score << std::endl;
-            std::cout << "Player 2 Score: " << m_Players[1].Score << std::endl;
-            std::cout << "Product: " << Product << std::endl;
-        } 
+            // Work with 0-based index
+            int startIndex = StartPos - 1;
+            int endIndex = (startIndex + RollValue) % 10;
+
+            // Convert back to 1-based
+            return uint8_t(endIndex + 1);
+        }
+
+        // Returns an end-position value for a given universe index
+        uint8_t QuantumTurn(uint8_t Position, uint8_t Universe)
+        {
+            // Construct a unique key from the possible input values
+            uint32_t key = Position + (uint32_t(Universe) << 8) ;
+            auto it = m_EndPosTable.find(key);
+            if (it == m_EndPosTable.end())
+            {
+                // Do the math
+                uint8_t rollResult;
+                rollResult = EndPos(Position, RollResults[Universe]);
+                m_EndPosTable.emplace(key, rollResult);
+                return rollResult;
+            }
+
+            return it->second;
+        }
+
+        // Returns the number of possible wins for each player as a pair
+        std::pair<size_t, size_t> Simulate(const std::pair<uint8_t, uint8_t> &Positions, std::pair<uint8_t, uint8_t> Scores)
+        {
+            std::pair<size_t, size_t> WinCounts(0, 0);
+            uint32_t key = Positions.first + (uint32_t(Positions.second) << 8) + (uint32_t(Scores.first) << 16) + (uint32_t(Scores.second) << 24);
+            auto it = m_WinCountsTable.find(key);
+            if (it == m_WinCountsTable.end())
+            {
+                // Do the math
+                for (uint8_t i = 0; i < NewUniversesPerTurn; ++i)
+                {
+                    std::pair<uint8_t, uint8_t> newPositions(QuantumTurn(Positions.first, i), Positions.second);
+                    std::pair<uint8_t, uint8_t> newScores(uint8_t(Scores.first + newPositions.first), Scores.second);
+
+                    if (newScores.first >= WinningScore)
+                    {
+                        WinCounts.first++;
+                    }
+                    else
+                    {
+                        for (uint8_t j = 0; j < NewUniversesPerTurn; ++j)
+                        {
+                            newPositions.second = QuantumTurn(Positions.second, j);
+                            newScores.second = Scores.second + newPositions.second;
+
+                            if (newScores.second >= WinningScore)
+                            {
+                                WinCounts.second++;
+                            }
+                            else
+                            {
+                                std::pair<size_t, size_t> InnerWinCounts = Simulate(newPositions, newScores);
+                                WinCounts.first += InnerWinCounts.first;
+                                WinCounts.second += InnerWinCounts.second;
+                            }
+                        }
+                    }
+                }
+                m_WinCountsTable.emplace(key, WinCounts);
+            }
+            else
+            {
+                WinCounts = it->second;
+            }
+
+            return WinCounts;
+        }
     };
 
     void Execute()
     {
-        Game game(6, 9);
-        game.Run();
+        Game game;
+        std::pair<size_t, size_t> WinCounts = game.Simulate(std::pair(Player1Start, Player2Start), std::make_pair<uint8_t, uint8_t>(0, 0));
+        std::cout << "Player 1 Wins: " << WinCounts.first << std::endl;
+        std::cout << "Player 2 Wins: " << WinCounts.second << std::endl;
     }
 }
