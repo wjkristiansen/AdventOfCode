b9437b9f73f54e29fe362c13cd7937e18744d8f8
William Kristiansen <wjkristiansen@hotmail.com>
2022-12-05 20:32:22 -0800

    2022: Day 3: Part 2: Solve Rucksack Reorganization

--- src/2022/Day3.cpp
+++ src/2022/Day3.cpp
@@ -19,6 +19,7 @@ void C2022Day3::Execute(const std::string &Name)
 
     size_t SumOfPriorities = 0;
 
+#if 0
     for(;!fstream.eof();)
     {
         std::string line;
@@ -41,6 +42,30 @@ void C2022Day3::Execute(const std::string &Name)
             }
         }
     }
+#else
+    for (; !fstream.eof();)
+    {
+        std::vector<int> elfMasks(52, 0);
+
+        for (int mask = 1; mask < (1 << 3); mask = mask << 1)
+        {
+            std::string line;
+            std::getline(fstream, line);
+
+            for (auto c : line)
+            {
+                auto p = Priority(c);
+                elfMasks[p - 1] |= mask;
+
+                if (elfMasks[p - 1] == 0x7)
+                {
+                    SumOfPriorities += p;
+                    break;
+                }
+            }
+        }
+    }
+#endif
 
     std::cout << "Sum of priorities = " << SumOfPriorities << std::endl;
 }
