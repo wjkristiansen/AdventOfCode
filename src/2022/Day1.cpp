12bd7ee09e1bd17ca081c2559ead261bf9bd447b
William Kristiansen <wjkristiansen@hotmail.com>
2022-12-05 16:40:34 -0800

    Implement Day1, 2022, Calorie Counting part2

--- src/2022/Day1.cpp
+++ src/2022/Day1.cpp
@@ -8,18 +8,8 @@ void C2022Day1::Execute(const std::string& Name)
 
     ReadData();
 
-    int Max = 0;
-    size_t Index = 0;
-    for (size_t i = 0; i < m_ElfCalories.size(); ++i)
-    {
-        if (m_ElfCalories[i] > Max)
-        {
-            Max = m_ElfCalories[i];
-            Index = i;
-        }
-    }
-
-    std::cout << "Elf with most calories is [" << Index << "] with " << Max << " Calories." << std::endl;
+    std::cout << "Elf with most calories is [" << m_MaxElf << "] with " << m_MaxCalories << " Calories." << std::endl;
+    std::cout << "Sum of top three elves calories is: " << m_Top3[0] + m_Top3[1] + m_Top3[2] << std::endl;
 }
 
 void C2022Day1::ReadData()
@@ -27,9 +17,9 @@ void C2022Day1::ReadData()
     std::ifstream fstream("Day1.txt");
     std::string line;
     
-    for(;;)
+    for(size_t i = 0;; ++i)
     {
-        m_ElfCalories.emplace_back(0);
+        size_t TotalCalories = 0;
 
         for(;;)
         {
@@ -39,8 +29,34 @@ void C2022Day1::ReadData()
                 break;
 
             int Calories = std::stoi(line);
-            m_ElfCalories.back() += Calories;
+            TotalCalories += Calories;
         }
+
+        if (TotalCalories > m_MaxCalories)
+        {
+            m_MaxCalories = TotalCalories;
+            m_MaxElf = i;
+        }
+
+        size_t LeastTop3 = 0;
+
+        if (m_Top3[0] < m_Top3[1])
+        {
+            if (m_Top3[0] < m_Top3[2])
+                LeastTop3 = 0;
+            else
+                LeastTop3 = 2;
+        }
+        else
+        {
+            if (m_Top3[1] < m_Top3[2])
+                LeastTop3 = 1;
+            else
+                LeastTop3 = 2;
+        }
+
+        if (TotalCalories > m_Top3[LeastTop3])
+            m_Top3[LeastTop3] = TotalCalories;
 
         if (fstream.eof())
             break;
--- src/Solutions.h
+++ src/Solutions.h
@@ -4,12 +4,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 #include "SolutionFactory.h"
 
 class C2022Day1 : public CSolutionBase
 {
     std::vector<int> m_ElfCalories;
+    size_t m_MaxCalories = 0;
+    size_t m_MaxElf = 0;
+
+    size_t m_Top3[3] = {};
 
 public:
     virtual void Execute(const std::string& Name);
