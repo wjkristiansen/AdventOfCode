7099593996eb49a1d5270ef0fb6f772869a15992
William Kristiansen <wjkristiansen@hotmail.com>
2022-12-11 16:41:10 -0800

    2022: Day 9: Part 2: Rope Bridge

--- src/2022/Day9.cpp
+++ src/2022/Day9.cpp
@@ -22,57 +22,53 @@ void CSolution<2022, 9>::Execute()
     std::ifstream fstream("Day9.txt");
     std::unordered_set<std::pair<int, int>> TailPosRecord;
 
-    std::pair<int, int> HeadPos{ 0, 0 };
-    std::pair<int, int> TailPos{ 0, 0 };
-    TailPosRecord.insert(TailPos);
+    const size_t NumKnots = 10;
+    std::vector<std::pair<int, int>> Knots(NumKnots, {0, 0});
+    TailPosRecord.insert({0, 0}); // Record the current tail position
 
-    auto MoveHead = [&HeadPos, &TailPos, &TailPosRecord](char Dir, int Dist)
+    auto MoveTail = [](const std::pair<int, int> &HeadPos, std::pair<int, int> &TailPos)
     {
-        for (int i = 0; i < Dist; ++i)
-        {
-            // Move the head by one
-            switch (Dir)
-            {
-            case 'U':
-                HeadPos.second -= 1;
-                break;
-            case 'D':
-                HeadPos.second += 1;
-                break;
-            case 'L':
-                HeadPos.first -= 1;
-                break;
-            case 'R':
-                HeadPos.first += 1;
-                break;
-            }
+        // Adjust the tail position
+        int dx = HeadPos.first - TailPos.first;
+        int dy = HeadPos.second - TailPos.second;
 
-            // Adjust the tail position
-            int dx = HeadPos.first - TailPos.first;
-            int dy = HeadPos.second - TailPos.second;
-            if (dx > 1)
+        if (dy >= -1 && dy <= 1)
+        {
+            assert(dx >= -2);
+            assert(dx <= 2);
+
+            if (dx == 2)
             {
                 TailPos.first = HeadPos.first - 1;
                 TailPos.second = HeadPos.second;
             }
-            else if (dx < -1)
+            else if (dx == -2)
             {
                 TailPos.first = HeadPos.first + 1;
                 TailPos.second = HeadPos.second;
             }
-            else if (dy > 1)
+        }
+        else if (dx >= -1 && dx <= 1)
+        {
+            assert(dy >= -2);
+            assert(dy <= 2);
+
+            if (dy == 2)
             {
                 TailPos.first = HeadPos.first;
                 TailPos.second = HeadPos.second - 1;
             }
-            else if (dy < -1)
+            else if (dy == -2)
             {
                 TailPos.first = HeadPos.first;
                 TailPos.second = HeadPos.second + 1;
             }
-
-            // Record the tail position
-            TailPosRecord.insert(TailPos);
+        }
+        else
+        {
+            // Not same row or column, so move by same delta
+            TailPos.first += dx > 0 ? 1 : -1;
+            TailPos.second += dy > 0 ? 1 : -1;
         }
     };
 
@@ -82,7 +78,49 @@ void CSolution<2022, 9>::Execute()
         char Dir;
         int Dist;
         fstream >> Dir >> Dist;
-        MoveHead(Dir, Dist);
+        for(int i = 0; i < Dist; ++i)
+        {
+            // Move the head by one
+            switch (Dir)
+            {
+            case 'U':
+                Knots[0].second += 1;
+                break;
+            case 'D':
+                Knots[0].second -= 1;
+                break;
+            case 'R':
+                Knots[0].first += 1;
+                break;
+            case 'L':
+                Knots[0].first -= 1;
+                break;
+            }
+
+            // Update the trailing knots
+            for(int j = 1; j < NumKnots; ++j)
+            {
+                MoveTail(Knots[j - 1], Knots[j]);
+            }
+
+            // Record the tail position
+            TailPosRecord.insert(Knots[NumKnots - 1]);
+        }
+    }
+
+    for (int j = 40; j > -30; )
+    {
+        --j;
+
+        for (int i = -40; i < 20; ++i )
+        {
+            auto it = TailPosRecord.find(std::pair<int, int>({ i, j }));
+            if (it == TailPosRecord.end())
+                std::cout << '.';
+            else
+                std::cout << '#';
+        }
+        std::cout << std::endl;
     }
 
     std::cout << "Tail Position Count: " << TailPosRecord.size() << std::endl;
