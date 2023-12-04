95eff35e2b7772fb46fe06af0ee97dad7a5fac44
Bill Kristiansen <billkris@microsoft.com>
2023-12-04 13:04:14 -0800

    2023: Day4: Solve Scratchcards part 2

--- src/2023/Day4.cpp
+++ src/2023/Day4.cpp
@@ -5,69 +5,142 @@ void CSolution<4>::Execute(int part)
 {
     std::ifstream fstream("2023/Day4.txt");
     std::string line;
-    int powerSum = 0;
-
     struct Card
     {
+        int Id;
+        int MatchCount;
+        int Copies;
         std::set<int> WinningNumbers;
         std::vector<int> PlayerNumbers;
 
-        int Power() const
+    private:
+        int CalcMatchCount() const
         {
-            int power = 0;
+            int matchCount = 0;
             for( int w : PlayerNumbers)
             {
                 if(WinningNumbers.find(w) != WinningNumbers.end())
                 {
-                    if( power == 0)
-                        power = 1;
-                    else
-                        power = power << 1;
+                    matchCount++;
                 }
             }
-            return power;
+            return matchCount;
+        }
+
+    public:
+        Card() = default;
+        Card(const Card&) = default;
+        Card(Card&& o)
+        {
+            Id = o.Id;
+            MatchCount = o.MatchCount;
+            Copies = o.Copies;
+            WinningNumbers = std::move(o.WinningNumbers);
+            PlayerNumbers = std::move(o.PlayerNumbers);
+            o.Id = 0;
+            o.MatchCount = 0;
+            o.Copies = 0;
+        }
+
+        int Read(std::istream& iss)
+        {
+            int cardId = 0;
+            iss.ignore(5, ' '); // "Card "
+            iss >> cardId;
+            iss.ignore(2, ' '); // ": "
+
+            // Read winning numbers
+            std::string numbers;
+            std::getline(iss, numbers, '|');
+            std::istringstream wss(numbers);
+            for (; !wss.eof();)
+            {
+                int number = 0;
+                wss >> number;
+                WinningNumbers.insert(number);
+            }
+
+            // Read player numbers
+            std::getline(iss, numbers);
+            std::istringstream pss(numbers);
+            for (; !pss.eof();)
+            {
+                int number = 0;
+                pss >> number;
+                PlayerNumbers.push_back(number);
+            }
+
+            MatchCount = CalcMatchCount();
+            Id = cardId;
+            Copies = 1;
+            return cardId;
+        }
+
+        int Power() const
+        {
+            int matchCount = MatchCount;
+            return matchCount == 0 ? 0 : 1 << (matchCount - 1);
         }
     };
 
-    for(;!fstream.eof();)
+    if (part == 1)
     {
-        std::getline(fstream, line);
-        if(line.empty())
-            break;
+        int powerSum = 0;
 
-        std::istringstream iss(line);
-        int cardId = 0;
-        iss.ignore(5, ' '); // "Card "
-        iss >> cardId;
-        iss.ignore(2, ' '); // ": "
-
-        Card card;
-
-        // Read winning numbers
-        std::string numbers;
-        std::getline(iss, numbers, '|');
-        std::istringstream wss(numbers);
-        for (; !wss.eof();)
+        for (; !fstream.eof();)
         {
-            int number = 0;
-            wss >> number;
-            card.WinningNumbers.insert(number);
+            std::getline(fstream, line);
+            if (line.empty())
+                break;
+
+            std::istringstream iss(line);
+
+            Card card;
+            int cardId = card.Read(iss);
+
+            int power = card.Power();
+            powerSum += power;
+            std::cout << "Card " << cardId << ": " << power << std::endl;
         }
 
-        // Read player numbers
-        std::getline(iss, numbers);
-        std::istringstream pss(numbers);
-        for (; !pss.eof();)
-        {
-            int number = 0;
-            pss >> number;
-            card.PlayerNumbers.push_back(number);
-        }
-        
-        int power = card.Power();
-        powerSum += power;
-        std::cout << "Card " << cardId << ": " << power << std::endl;
+        std::cout << "Sum of card powers: " << powerSum << std::endl;
     }
+    else
+    {
+        std::map<int, Card> Cards;
+        for (; !fstream.eof();)
+        {
+            std::getline(fstream, line);
+            if (line.empty())
+                break;
 
-    std::cout << "Sum of card powers: " << powerSum << std::endl;
+            std::istringstream iss(line);
+            Card card;
+            int cardId = card.Read(iss);
+
+            Cards.emplace(cardId, std::move(card));
+        }
+
+        // Calculate total card count
+        int totalCards = 0;
+        for (auto& [_, card] : Cards)
+        {
+            // Fixup copy counts of subsequent cards
+            for (int i = 0; i < card.MatchCount; ++i)
+            {
+                auto it = Cards.find(card.Id + i + 1);
+                if (it != Cards.end())
+                {
+					it->second.Copies += card.Copies;
+				}
+            }
+
+            std::cout << "Card: Id: " << card.Id << ", Copies: " << card.Copies << std::endl;
+
+            // Add to total
+            totalCards += card.Copies;
+		}
+
+        std::cout << "Total Cards: " << totalCards << std::endl;
+    }
 }
