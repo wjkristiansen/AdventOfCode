d9537ef2d65d9bc457f2be375b50995682c5451e
William Kristiansen <wjkristiansen@hotmail.com>
2024-12-19 08:33:53 -0800

    2024: Day 12: Garden Groups, part 2

--- src/2024/Day12.cpp
+++ src/2024/Day12.cpp
@@ -10,6 +10,7 @@ public:
 
     AsciiGrid m_Grid;
     Grid<int> m_Mask;
+    size_t m_NumSides = 0;
 
     std::pair<size_t, size_t> ComputeRegion(size_t startRow, size_t startCol, char c)
     {
@@ -37,6 +38,8 @@ public:
             if (0 != m_Mask.GetValue(row, col))
                 continue; // Been here already
 
+            m_NumSides += NumEdgeStarts(row, col);
+
             // Mark the associated mask location
             m_Mask.SetValue(1, row, col);
             ++regionSize;
@@ -48,6 +51,65 @@ public:
         }
 
         return std::pair(regionSize, perimeter);
+    }
+
+    int NumEdgeStarts(size_t row, size_t col)
+    {
+        // An edge starts at a corner. There are multiple
+        // geometries that represent a corner:
+        // Assume edges are counted clockwise, the 
+        // ASCII drawing below shows all possible
+        // edge orientations.
+        //
+        //     ^--->
+        //     | X |
+        // ^--->   v--->
+        // | X   X   X |
+        // <---^   <---v
+        //     | X |
+        //     <---v
+        //
+        // An edge must *start* at a give location, so
+        // vertically or horizontally adjacent plots
+        // do not count as an edge.
+        //
+        // ^----------->
+        // | X   X   X |
+        // <---^   <---v
+        //     | X |
+        //     <---v
+        //
+
+        char c = m_Grid.GetValue(row, col);
+
+        bool leftEdge = (col == 0 || m_Grid.GetValue(row, col - 1) != c);
+        bool topEdge = (row == 0 || m_Grid.GetValue(row - 1, col) != c);
+        bool rightEdge = (col == m_Grid.Width() - 1 || m_Grid.GetValue(row, col + 1) != c);
+        bool bottomEdge = (row == m_Grid.Height() - 1 || m_Grid.GetValue(row + 1, col) != c);
+
+        int numEdgeStarts = 0;
+
+        // Convex edges
+        if (leftEdge && topEdge)
+            numEdgeStarts++; // Start of a convex top edge
+        if (topEdge && rightEdge)
+            numEdgeStarts++; // Start of a convex right edge
+        if (rightEdge && bottomEdge)
+            numEdgeStarts++; // Start of a convex bottom edge
+        if (bottomEdge && leftEdge)
+            numEdgeStarts++; // Start of a convex left edge
+
+        // Concave edges
+        if (!leftEdge && !topEdge && m_Grid.GetValue(row - 1, col - 1) != c)
+            numEdgeStarts++; // Start of concave left edge
+        if (!topEdge && !rightEdge && m_Grid.GetValue(row - 1, col + 1) != c)
+            numEdgeStarts++; // Start of concave top edge
+        if (!rightEdge && !bottomEdge && m_Grid.GetValue(row + 1, col + 1) != c)
+            numEdgeStarts++; // Start of concave right edge
+        if (!bottomEdge && !leftEdge && m_Grid.GetValue(row + 1, col - 1) != c)
+            numEdgeStarts++; // Start of concave bottom edge
+
+        return numEdgeStarts;
     }
 
     virtual void Execute(int part)
@@ -73,10 +135,32 @@ public:
                     cost += regionSize * perimeter;
                 }
             }
-            std::cout << "Total Cost: " << cost << std::endl;
+            std::cout << "Total Cost: " << cost << std::endl;    
         }
         else
         {
+            m_Grid.ReadFromFile("2024/Day12.txt");
+            m_Mask = Grid<int>(m_Grid.Width(), m_Grid.Height(), 0);
+            m_Grid.Print();
+            std::cout << std::endl;
+            m_Mask.Print();
+
+            size_t cost = 0;
+            for (size_t row = 0; row < m_Grid.Height(); ++row)
+            {
+                for (size_t col = 0; col < m_Grid.Width(); ++col)
+                {
+                    if (m_Mask.GetValue(row, col) != 0)
+                        continue;
+                    size_t initNumSides = m_NumSides;
+                    char c = m_Grid.GetValue(row, col);
+                    auto [regionSize, perimeter] = ComputeRegion(row, col, c);
+                    size_t regionSides = m_NumSides - initNumSides;
+                    std::cout << "Region " << c << ": Size=" << regionSize << ", Sides=" << regionSides << std::endl;
+                    cost += regionSize * regionSides;
+                }
+            }
+            std::cout << "Total Cost: " << cost << std::endl;
         }
     }
 };
